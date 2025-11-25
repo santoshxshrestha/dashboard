9a6c59d99fd8ceffa1d0ac55279d772ab17ea219
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-26 00:53:38 +0545

    refactor: need to use some of the debouncing to prevent the server from using all the mem

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {useDebouncedCallback} from 'use-debounce';
 import { useSearchParams } from 'next/navigation';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
-    const {replace } = useRouter();
-
-    function handleSearch(term: string) {
+    const {replace} = useRouter()
+    const handleSearch = useDebouncedCallback( (term) => {
         console.log(`Searching...${term}`)
         const params = new URLSearchParams(searchParams);
         if (term) {
@@ -19,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-    }
+    },300)
 
     return (
         <div className="relative flex flex-1 flex-shrink-0">
