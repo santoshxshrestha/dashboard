456b04dd2f74cbccb8e3924a4605461f2b891390
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-27 01:23:54 +0545

    create invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,32 @@
 "use server";
+import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
-  };
-  console.log(rawFormData);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+  await sql`
+        insert into invoices (customer_id, amount, status, date) values (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  // once the invoice is created, revalidate the invoices path to show the new invoice
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
