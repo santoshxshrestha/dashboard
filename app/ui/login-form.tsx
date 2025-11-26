b11546e1b06c785f0f717a399a79eb53a3ba2cb9
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-27 01:02:16 +0545

    form impl

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,3 +1,4 @@
+import { createInvoice } from "../lib/actions";
 import { lusitana } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
@@ -9,7 +10,7 @@ import { Button } from "./button";
 
 export default function LoginForm() {
   return (
-    <form className="space-y-3">
+    <form className="space-y-3" action={createInvoice}>
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
